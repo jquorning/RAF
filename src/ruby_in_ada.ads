89a45f3a3e81ceff7c4dc953b73ce15c4ad43c3c
Jesper Quorning <jesper.quorning@gmail.com>
2023-01-08 22:20:24 +0100

    Update ruby_in_ada.ads

--- src/ruby_in_ada.ads
+++ src/ruby_in_ada.ads
@@ -1,6 +1,19 @@
 package Ruby_In_Ada
 is
    type Object is new Integer;
-   function New_Symbol (Name : String) return Object;
+   
+   Topic_Access : access Object := null;
+
+   Topic : access Object renames Topic_Access.all;
+   --  Result of last operatiom goes here.
+
+   type Integer renames Standard.Integer;
+   type Float   renames Standard.Lomg_Float;
+   type String  renames Standard.String;
+   
+   procedure New_Symbol  (Name : String);
+   procedure New_String  (Item : String);
+   procedure New_Integer (Value : Integer);
+   procedure New_Float   (Value : Float);
 
 end Ruby_In_Ada;
